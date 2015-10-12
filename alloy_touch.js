f21b452193455e644128dac4120af405f675bd6f
张磊 <kmdjs@qq.com>
2015-10-12 19:08:53 +0800

    all in one cmt

---
+++ alloy_touch.js
@@ -0,0 +1,305 @@
+﻿; (function () {
+    (function () {
+        var lastTime = 0;
+        var vendors = ['webkit', 'moz'];
+        for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+            window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+            window.cancelAnimationFrame =
+              window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
+        }
+
+        if (!window.requestAnimationFrame)
+            window.requestAnimationFrame = function (callback, element) {
+                var currTime = new Date().getTime();
+                var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+                var id = window.setTimeout(function () { callback(currTime + timeToCall); },
+                  timeToCall);
+                lastTime = currTime + timeToCall;
+                return id;
+            };
+
+        if (!window.cancelAnimationFrame)
+            window.cancelAnimationFrame = function (id) {
+                clearTimeout(id);
+            };
+    }());
+
+    //many thanks to https://github.com/gre/bezier-easing
+    var NEWTON_ITERATIONS = 4;
+    var NEWTON_MIN_SLOPE = 0.001;
+    var SUBDIVISION_PRECISION = 0.0000001;
+    var SUBDIVISION_MAX_ITERATIONS = 10;
+
+    var kSplineTableSize = 11;
+    var kSampleStepSize = 1.0 / (kSplineTableSize - 1.0);
+
+    var float32ArraySupported = typeof Float32Array === "function";
+
+    function A(aA1, aA2) { return 1.0 - 3.0 * aA2 + 3.0 * aA1; }
+    function B(aA1, aA2) { return 3.0 * aA2 - 6.0 * aA1; }
+    function C(aA1) { return 3.0 * aA1; }
+
+    // Returns x(t) given t, x1, and x2, or y(t) given t, y1, and y2.
+    function calcBezier(aT, aA1, aA2) {
+        return ((A(aA1, aA2) * aT + B(aA1, aA2)) * aT + C(aA1)) * aT;
+    }
+
+    // Returns dx/dt given t, x1, and x2, or dy/dt given t, y1, and y2.
+    function getSlope(aT, aA1, aA2) {
+        return 3.0 * A(aA1, aA2) * aT * aT + 2.0 * B(aA1, aA2) * aT + C(aA1);
+    }
+
+    function binarySubdivide(aX, aA, aB, mX1, mX2) {
+        var currentX, currentT, i = 0;
+        do {
+            currentT = aA + (aB - aA) / 2.0;
+            currentX = calcBezier(currentT, mX1, mX2) - aX;
+            if (currentX > 0.0) {
+                aB = currentT;
+            } else {
+                aA = currentT;
+            }
+        } while (Math.abs(currentX) > SUBDIVISION_PRECISION && ++i < SUBDIVISION_MAX_ITERATIONS);
+        return currentT;
+    }
+
+    function newtonRaphsonIterate(aX, aGuessT, mX1, mX2) {
+        for (var i = 0; i < NEWTON_ITERATIONS; ++i) {
+            var currentSlope = getSlope(aGuessT, mX1, mX2);
+            if (currentSlope === 0.0) return aGuessT;
+            var currentX = calcBezier(aGuessT, mX1, mX2) - aX;
+            aGuessT -= currentX / currentSlope;
+        }
+        return aGuessT;
+    }
+
+    /**
+     * points is an array of [ mX1, mY1, mX2, mY2 ]
+     */
+    function BezierEasing(points, b, c, d) {
+        if (arguments.length === 4) {
+            return new BezierEasing([points, b, c, d]);
+        }
+        if (!(this instanceof BezierEasing)) return new BezierEasing(points);
+
+        if (!points || points.length !== 4) {
+            throw new Error("BezierEasing: points must contains 4 values");
+        }
+        for (var i = 0; i < 4; ++i) {
+            if (typeof points[i] !== "number" || isNaN(points[i]) || !isFinite(points[i])) {
+                throw new Error("BezierEasing: points should be integers.");
+            }
+        }
+        if (points[0] < 0 || points[0] > 1 || points[2] < 0 || points[2] > 1) {
+            throw new Error("BezierEasing x values must be in [0, 1] range.");
+        }
+
+        this._str = "BezierEasing(" + points + ")";
+        this._css = "cubic-bezier(" + points + ")";
+        this._p = points;
+        this._mSampleValues = float32ArraySupported ? new Float32Array(kSplineTableSize) : new Array(kSplineTableSize);
+        this._precomputed = false;
+
+        this.get = this.get.bind(this);
+    }
+
+    BezierEasing.prototype = {
+
+        get: function (x) {
+            var mX1 = this._p[0],
+              mY1 = this._p[1],
+              mX2 = this._p[2],
+              mY2 = this._p[3];
+            if (!this._precomputed) this._precompute();
+            if (mX1 === mY1 && mX2 === mY2) return x; // linear
+            // Because JavaScript number are imprecise, we should guarantee the extremes are right.
+            if (x === 0) return 0;
+            if (x === 1) return 1;
+            return calcBezier(this._getTForX(x), mY1, mY2);
+        },
+
+        getPoints: function () {
+            return this._p;
+        },
+
+        toString: function () {
+            return this._str;
+        },
+
+        toCSS: function () {
+            return this._css;
+        },
+
+        // Private part
+
+        _precompute: function () {
+            var mX1 = this._p[0],
+              mY1 = this._p[1],
+              mX2 = this._p[2],
+              mY2 = this._p[3];
+            this._precomputed = true;
+            if (mX1 !== mY1 || mX2 !== mY2)
+                this._calcSampleValues();
+        },
+
+        _calcSampleValues: function () {
+            var mX1 = this._p[0],
+              mX2 = this._p[2];
+            for (var i = 0; i < kSplineTableSize; ++i) {
+                this._mSampleValues[i] = calcBezier(i * kSampleStepSize, mX1, mX2);
+            }
+        },
+
+        /**
+         * getTForX chose the fastest heuristic to determine the percentage value precisely from a given X projection.
+         */
+        _getTForX: function (aX) {
+            var mX1 = this._p[0],
+              mX2 = this._p[2],
+              mSampleValues = this._mSampleValues;
+
+            var intervalStart = 0.0;
+            var currentSample = 1;
+            var lastSample = kSplineTableSize - 1;
+
+            for (; currentSample !== lastSample && mSampleValues[currentSample] <= aX; ++currentSample) {
+                intervalStart += kSampleStepSize;
+            }
+            --currentSample;
+
+            // Interpolate to provide an initial guess for t
+            var dist = (aX - mSampleValues[currentSample]) / (mSampleValues[currentSample + 1] - mSampleValues[currentSample]);
+            var guessForT = intervalStart + dist * kSampleStepSize;
+
+            var initialSlope = getSlope(guessForT, mX1, mX2);
+            if (initialSlope >= NEWTON_MIN_SLOPE) {
+                return newtonRaphsonIterate(aX, guessForT, mX1, mX2);
+            } else if (initialSlope === 0.0) {
+                return guessForT;
+            } else {
+                return binarySubdivide(aX, intervalStart, intervalStart + kSampleStepSize, mX1, mX2);
+            }
+        }
+    };
+
+
+    function bind(element, type, callback) {
+        element.addEventListener(type, callback, false);
+    }
+
+    //http://kmdjs.github.io/dnt/demo43/index.html
+    function iosEase(x) {
+        return Math.sqrt(1 - Math.pow(x - 1, 2));
+    }
+
+    window.AlloyTouch = function (option) {
+        var scroller = option.target,
+            element = typeof option.touch==="string"?document.querySelector(option.touch):option.touch,
+            vertical = option.vertical === undefined ? true : option.vertical,
+            property = option.property,
+            tickID = 0,
+            preX,
+            preY,
+            factor = option.factor === undefined ? 1 : option.factor,
+            //拖动时候的摩擦因子
+            factor1 = 1,
+            min = option.min,
+            max = option.max,
+            startTime,
+            start,
+            easing = BezierEasing(0.1, 0.57, 0.1, 1),
+            recording = false,
+            deceleration = 0.0006,
+            change = option.change || function () { };
+
+        var hasMin = !(min === undefined),
+            hasMax = !(max === undefined);
+            
+        bind(element, "touchstart", function (evt) {
+            cancelAnimationFrame(tickID);        
+            startTime = new Date().getTime();
+            preX = evt.touches[0].pageX;
+            preY = evt.touches[0].pageY;
+            start = vertical ? preY : preX;
+            evt.preventDefault();
+        })
+
+        bind(element, "touchmove", function (evt) {
+            var d = (vertical ? evt.touches[0].pageY - preY : evt.touches[0].pageX - preX) * factor;
+            if (hasMax&&scroller[property] > max && d > 0) {
+                factor1 = 0.3;
+            } else if (hasMin&&scroller[property] < min && d < 0) {
+                factor1 = 0.3;
+            } else {
+                factor1 = 1;
+            }
+            d *= factor1;
+            preX = evt.touches[0].pageX;
+            preY = evt.touches[0].pageY;
+            scroller[property] += d;
+            change(scroller[property]);
+            var timestamp = new Date().getTime();
+            if (timestamp - startTime > 300) {
+                startTime = timestamp;
+                start = vertical ? preY : preX;
+            }
+            evt.preventDefault();
+        })
+
+        bind(document, "touchend", function (evt) {
+            if (hasMax && scroller[property] > max) {
+                to(scroller, property, max, 200, iosEase);
+            } else if (hasMin && scroller[property] < min) {
+                to(scroller, property, min, 200, iosEase);
+            }else  {
+                //var y = evt.changedTouches[0].pageY;
+                var duration = new Date().getTime() - startTime;
+                if (duration < 300) {
+                    var distance = ((vertical ? evt.changedTouches[0].pageY : evt.changedTouches[0].pageX) - start) * factor,
+                        speed = Math.abs(distance) / duration,
+                        destination = scroller[property] + (speed * speed) / (2 * deceleration) * (distance < 0 ? -1 : 1);
+                    to(scroller, property, Math.round(destination), Math.round(speed / deceleration), easing.get);
+                }
+            }
+            evt.preventDefault();
+        })
+       
+        function to(el, property, value, time, ease, callback) {
+
+            var current = el[property];
+            var dv = value - current;
+            var beginTime = new Date();
+
+            var toTick = function () {
+                var dt = new Date() - beginTime;
+                if (dt >= time) {
+                    el[property] = value;
+                    change(el[property]);
+                    callback && callback();
+                    return;
+                }
+                el[property] = Math.round(dv * ease(dt / time) + current);
+                change(el[property]);
+                tickID = requestAnimationFrame(toTick);
+                if (!recording) {
+                    if (hasMax && el[property] > max) {
+                        recording = true;
+                        setTimeout(function () {
+                            cancelAnimationFrame(tickID);
+                            recording = false;
+                            to(el, property, max, 200, iosEase);
+                        }, 50);
+                    } else if (hasMin && el[property] < min) {
+                        recording = true;
+                        setTimeout(function () {
+                            cancelAnimationFrame(tickID);
+                            recording = false;
+                            to(el, property, min, 200, iosEase);
+                        }, 50);
+                    }
+                }
+            }
+            toTick();
+        }
+    }
+})();
