ee3adde24367c67a45fc279c0c183df9cda3963f
张磊 <kmdjs@qq.com>
2015-10-15 15:47:16 +0800

    all in one cmt

--- transform.js
+++ transform.js
@@ -227,19 +227,17 @@
     });
 
     window.Transform = function (element) {
-            element.perspective = 400;
-            element.scaleX = element.scaleY = element.scaleZ = 1;
-            element.x = element.y = element.z = element.rotateX = element.rotateY = element.rotateZ = element.regX = element.regY = element.skewX = element.skewY = element.regX = element.regY = element.regZ = 0;
-            element.matrix3D = new matrix3D();
-            var observer = observable.watch(element, ["x", "y", "z", "scaleX", "scaleY", "scaleZ", "perspective", "rotateX", "rotateY", "rotateZ", "regX", "regY", "regZ"]);
+        element.perspective = 400;
+        element.scaleX = element.scaleY = element.scaleZ = 1;
+        element.x = element.y = element.z = element.rotateX = element.rotateY = element.rotateZ = element.regX = element.regY = element.skewX = element.skewY = element.regX = element.regY = element.regZ = 0;
+        element.matrix3D = new matrix3D();
+        var observer = observable.watch(element, ["x", "y", "z", "scaleX", "scaleY", "scaleZ", "perspective", "rotateX", "rotateY", "rotateZ", "regX", "regY", "regZ"]);
 
-            this.element = element;
-            var self = this;
-            observer.propertyChangedHandler = function () {
-                var mtx = self.element.matrix3D.identity().appendTransform(self.element.perspective, self.element.x, self.element.y, self.element.z, self.element.scaleX, self.element.scaleY, self.element.scaleZ, self.element.rotateX, self.element.rotateY, self.element.rotateZ, self.element.regX, self.element.regY, self.element.regZ);
+        observer.propertyChangedHandler = function () {
+            var mtx = element.matrix3D.identity().appendTransform(element.perspective, element.x, element.y, element.z, element.scaleX, element.scaleY, element.scaleZ, element.rotateX, element.rotateY, element.rotateZ, element.regX, element.regY, element.regZ);
 
-                self.element.style.transform = self.element.style.msTransform = self.element.style.OTransform = self.element.style.MozTransform = self.element.style.webkitTransform = "matrix3d(" + Array.prototype.slice.call(mtx.elements).join(",") + ")";
-            }
+            element.style.transform = element.style.msTransform = element.style.OTransform = element.style.MozTransform = element.style.webkitTransform = "matrix3d(" + Array.prototype.slice.call(mtx.elements).join(",") + ")";
         }
+    }
 
 })();
