d2ad32dfa241ce95a57584595a29a826409eae5b
张磊 <kmdjs@qq.com>
2015-12-21 10:12:51 +0800

    统一ease

--- alloy_touch.js
+++ alloy_touch.js
@@ -29,42 +29,6 @@
             };
     }());
 
-    //many thanks to http://greweb.me/2012/02/bezier-curve-based-easing-functions-from-concept-to-implementation/
-    function KeySpline(mX1, mY1, mX2, mY2) {
-
-        this.get = function (aX) {
-            if (mX1 == mY1 && mX2 == mY2) return aX; // linear
-            return CalcBezier(GetTForX(aX), mY1, mY2);
-        }
-
-        function A(aA1, aA2) { return 1.0 - 3.0 * aA2 + 3.0 * aA1; }
-        function B(aA1, aA2) { return 3.0 * aA2 - 6.0 * aA1; }
-        function C(aA1) { return 3.0 * aA1; }
-
-        // Returns x(t) given t, x1, and x2, or y(t) given t, y1, and y2.
-        function CalcBezier(aT, aA1, aA2) {
-            return ((A(aA1, aA2) * aT + B(aA1, aA2)) * aT + C(aA1)) * aT;
-        }
-
-        // Returns dx/dt given t, x1, and x2, or dy/dt given t, y1, and y2.
-        function GetSlope(aT, aA1, aA2) {
-            return 3.0 * A(aA1, aA2) * aT * aT + 2.0 * B(aA1, aA2) * aT + C(aA1);
-        }
-
-        function GetTForX(aX) {
-            // Newton raphson iteration
-            var aGuessT = aX;
-            for (var i = 0; i < 4; ++i) {
-                var currentSlope = GetSlope(aGuessT, mX1, mX2);
-                if (currentSlope == 0.0) return aGuessT;
-                var currentX = CalcBezier(aGuessT, mX1, mX2) - aX;
-                aGuessT -= currentX / currentSlope;
-            }
-            return aGuessT;
-        }
-    }
-
-
     function bind(element, type, callback) {
         element.addEventListener(type, callback, false);
     }
@@ -101,7 +65,6 @@
         this.max = option.max;
         this.startTime;
         this.start;
-        this.easing = new KeySpline(0.1, 0.57, 0.1, 1);
         this.recording = false;
         this.deceleration = 0.0006;
         this.change = option.change || function () { };
@@ -197,7 +160,7 @@
                             speed2 = this.factor * speed,
                             destination = this.scroller[this.property] + (speed2 * speed2) / (2 * this.deceleration) * (distance < 0 ? -1 : 1);
 
-                        self.to(this.scroller, this.property, Math.round(destination), Math.round(speed / self.deceleration), self.easing.get, function (value) {
+                        self.to(this.scroller, this.property, Math.round(destination), Math.round(speed / self.deceleration), iosEase, function (value) {
 
                             if (self.spring) {
                                 if (self.hasMax && self.scroller[self.property] > self.max) {
