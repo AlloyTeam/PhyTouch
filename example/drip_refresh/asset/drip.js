736390f772c433876a88b58fbdf7574350cc863d
kmdjs <kmdjs@qq.com>
2016-12-05 13:24:15 +0800

    clear up

--- example/drip_refresh/asset/drip.js
+++ example/drip_refresh/asset/drip.js
@@ -6,31 +6,17 @@ var Drip = function(selector , r){
     this.y = r+1;
     this.r = r;
     this.distance = 0;
-    this.drawBigCircle();
-    this.drawSmallCircle();
-    this.drawTransition();
+    this.draw();
 };
 
 Drip.prototype = {
-    drawBigCircle:function(){
+    draw:function(){
+        if( this.visible === false)return;
         this.ctx.beginPath();
         this.ctx.arc(this.x, this.y,  this.r-this.distance/7, -Math.PI,0, false);
-
-
-    },
-    drawSmallCircle:function(){
-
-       // this.ctx.arc(this.x, this.y+ this.distance,  this.r-this.distance/4, 0,Math.PI, false);
-
-
-    },
-    drawTransition:function(){
         this.ctx.quadraticCurveTo(this.x+this.r-this.distance/4,this.y+this.distance/2,this.x+this.r-this.distance/4,this.y+this.distance);
         this.ctx.arc(this.x, this.y+ this.distance,  this.r-this.distance/4, 0,Math.PI, false);
-
         this.ctx.quadraticCurveTo(this.x-this.r+this.distance/4,this.y+this.distance/2,this.x-this.r+this.distance/7,this.y);
-
-
         this.ctx.closePath();
         this.ctx.fill();
     },
@@ -39,17 +25,10 @@ Drip.prototype = {
         this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
         this.draw();
     },
-    draw:function(){
-        if( this.visible === false)return;
-        this.drawBigCircle();
-        this.drawSmallCircle();
-        this.drawTransition();
-    },
     hide:function(){
         this.visible = false;
         this.distance = 0;
         this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
-
     },
     show:function(){
         this.visible = true;
