191caf1acb69d1da205836c0ad125af0744c129e
june_01 <juneandgreen@gmail.com>
2016-12-16 10:33:42 +0800

    add some fixed

--- alloy_touch.select.multiple.js
+++ alloy_touch.select.multiple.js
@@ -1,20 +1,23 @@
 (function () {
+    if(!AlloyTouch) throw new Error('you need to include AlloyTouch!');
 
     AlloyTouch.MultipleSelect = function (option) {
-        this.renderTo = typeof option.renderTo === "string" ? document.querySelector(option.renderTo) : option.renderTo;
+        this.renderTo = typeof option.renderTo === 'string' ? document.querySelector(option.renderTo) : option.renderTo;
         this.length = option.options.length;
+        this.options = option.options;
+
+        var that = this;
         
         var i = 0;
         var arrProv = [],arrCity=[];
         for (; i < this.length; i++) {
-            var item =option.options[i];
+            var item =this.options[i];
             if (option.selectedIndex[0] === i) {
                 arrCity = item.list;
             }
-            arr1.push('<li>' + item.name + '</li>');
         }
 
-        var tpl = '    <div class="alloy-selector">\
+        var tpl = '<div class="alloy-selector">\
                     <div class="alloy-selector-mask"></div>\
                     <div class="alloy-selector-wrap">\
                         <div class="alloy-selector-head jmu-border-1px border-bottom">\
@@ -24,7 +27,7 @@
                             <div class="alloy-selector-submask1 jmu-border-1px border-bottom"></div>\
                             <div class="alloy-selector-submask2 jmu-border-1px border-top"></div>\
                             <div id="alloy-selector-line-1-0" class="alloy-selector-line">\
-                                <ul>' + arrProv.join("")+ '</ul>\
+                                <ul></ul>\
                             </div>\
                             <div id="alloy-selector-line-1-1" class="alloy-selector-line">\
                                 <ul></ul>\
@@ -33,10 +36,12 @@
                             <div class="alloy-selector-right"></div>\
                         </div>\
                     </div>\
-                </div>'
+                </div>';
+
+        this.renderTo.insertAdjacentHTML('beforeEnd', tpl);
 
        
-        for (var p in cityData) {
+        for (var p in this.options) {
             arrProv.push(p);
          
         }
@@ -45,43 +50,47 @@
         var _selectedC = '';
         var _p2cCache = {};
         // mask.style.height = window.innerHeight + "px";
-        var completeBtn = document.getElementById("alloy-selector-complete-button-1");
+        
+        // enter btn
+        var completeBtn = document.getElementById('alloy-selector-complete-button-1');
         completeBtn.addEventListener('touchend', function (e) {
             alert(_selectedP + '-' + _selectedC);
             e.stopPropagation();
             e.preventDefault();
         }, false);
-        var list = document.querySelectorAll(".alloy-selector-line");
+
+        var list = document.querySelectorAll('.alloy-selector-line');
         var list1 = list[0];
         var arr1 = [];
         var arrProv = [];
-        for (var p in cityData) {
+        for (var p in this.options) {
             if (!_selectedP) {
                 _selectedP = p;
             }
             arrProv.push(p);
             arr1.push('<li>' + p + '</li>');
         }
-        list1.querySelector("ul").innerHTML = arr1.join('');
+        list1.querySelector('ul').innerHTML = arr1.join('');
         var list2 = list[1];
-        var cityInfo = _getCityList();
+        var cityInfo = _getCityList.call(that);
         _selectedC = cityInfo[0];
-        list2.querySelector("ul").innerHTML = cityInfo.content;
-        var leftDiv = document.querySelector(".alloy-selector-left");
-        var rightDiv = document.querySelector(".alloy-selector-right");
+        list2.querySelector('ul').innerHTML = cityInfo.content;
+        var leftDiv = document.querySelector('.alloy-selector-left');
+        var rightDiv = document.querySelector('.alloy-selector-right');
+
         Transform(list1);
         Transform(list2);
         var at1 = new AlloyTouch({
-            touch: leftDiv,//反馈触摸的dom
-            target: list1, //运动的对象
-            property: "translateY",  //被滚动的属性
-            min: -33 * (arrProv.length - 1), //不必需,滚动属性的最小值
+            touch: leftDiv, // 反馈触摸的dom
+            target: list1, // 运动的对象
+            property: "translateY",  // 被滚动的属性
+            min: -33 * (arrProv.length - 1), // 不必需,滚动属性的最小值
             max: 0,
             animationEnd: function (value) {
                 console.log(value)
                 _selectedP = arrProv[Math.round(Math.abs(value / 33))];
                 console.log(Math.abs(value / 33))
-                var cityInfo = _getCityList();
+                var cityInfo = _getCityList.call(that);
 
                 _selectedC = cityInfo[0];
                 list2.querySelector("ul").innerHTML = cityInfo.content;
@@ -91,13 +100,13 @@
             step: 33
         });
         var at2 = new AlloyTouch({
-            touch: rightDiv,//反馈触摸的dom
-            target: list2, //运动的对象
-            property: "translateY",  //被滚动的属性
-            min: -33 * (cityInfo.length - 1), //不必需,滚动属性的最小值
+            touch: rightDiv, // 反馈触摸的dom
+            target: list2, // 运动的对象
+            property: "translateY",  // 被滚动的属性
+            min: -33 * (cityInfo.length - 1), // 不必需,滚动属性的最小值
             max: 0,
             animationEnd: function (value) {
-                _selectedC = _getCityList()[Math.round(Math.abs(value / 33))];
+                _selectedC = _getCityList.call(that)[Math.round(Math.abs(value / 33))];
                 //console.log(_selectedC);
             },
             step: 33
@@ -108,8 +117,8 @@
             if (!_p2cCache[_selectedP]) {
                 var arr = [];
                 var info = {};
-                for (var i = 0; i < cityData[_selectedP].length; ++i) {
-                    info[i] = cityData[_selectedP][i];
+                for (var i = 0; i < this.options[_selectedP].length; ++i) {
+                    info[i] = this.options[_selectedP][i];
                     arr.push('<li>' + info[i] + '</li>');
                 }
                 info.length = i;
