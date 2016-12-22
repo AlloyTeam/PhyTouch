38208d45a32153aae0eb97c5b0dab4554958cd9b
june_01 <juneandgreen@gmail.com>
2016-12-22 17:29:27 +0800

    add css for multiple select

--- alloy_touch.select.multiple.js
+++ alloy_touch.select.multiple.js
@@ -3,6 +3,207 @@
 
     if(!Transform) throw new Error('you need to include Transform!');
 
+    function getCss(level) {
+        var t = '';
+        var w = 100/level;
+        for(var i=0; i<level; i++) {
+            t += '.alloy-selector-touch' + i + ' {\
+                  position: absolute;\
+                  left: ' + i*w + '%;\
+                  top: 0px;\
+                  width: ' + w + '%;\
+                  height: 500px;\
+                  z-index: 1002;\
+                }';
+        }
+
+        return '* {\
+                  -webkit-tap-highlight-color: transparent;\
+                  box-sizing: border-box;\
+                }\
+                .jmu-border-1px {\
+                  position: relative;\
+                }\
+                .jmu-border-1px:after {\
+                  display: block;\
+                  position: absolute;\
+                  top: 0;\
+                  right: 0;\
+                  bottom: 0;\
+                  left: 0;\
+                  -webkit-transform: scale(1);\
+                  transform: scale(1);\
+                  -webkit-transform-origin: 0 0;\
+                  transform-origin: 0 0;\
+                  content: "";\
+                  pointer-events: none;\
+                }\
+                @media only screen and (-webkit-min-device-pixel-ratio: 2) {\
+                  .jmu-border-1px:after {\
+                    right: -100%;\
+                    bottom: -100%;\
+                    -webkit-transform: scale(0.5);\
+                    transform: scale(0.5);\
+                  }\
+                }\
+                .jmu-border-1px.border-bottom:after {\
+                  border-bottom: 1px solid #cdcdcd;\
+                }\
+                .jmu-border-1px.border-top:after {\
+                  border-top: 1px solid #cdcdcd;\
+                }\
+                .alloy-selector {\
+                  overflow: hidden;\
+                  position: fixed;\
+                  top: 0;\
+                  left: 0;\
+                  bottom: 0;\
+                  right: 0;\
+                  z-index: 10000;\
+                  padding: 0;\
+                }\
+                .alloy-selector-wrap {\
+                  position: absolute;\
+                  bottom: 0;\
+                  left: 0;\
+                  z-index: 1000;\
+                  width: 100%;\
+                  height: 256px;\
+                  background-color: #fff;\
+                  -webkit-transition: .3s ease-out all;\
+                  transition: .3s ease-out all;\
+                }\
+                .alloy-selector-head {\
+                  overflow: hidden;\
+                  border-color: #bbb;\
+                  padding: 9px;\
+                  height: 44px;\
+                }\
+                .alloy-selector-button {\
+                  display: block;\
+                  float: right;\
+                  width: 50px;\
+                  font-size: 17px;\
+                  line-height: 26px;\
+                  text-align: right;\
+                  color: #1d79eb;\
+                  outline: 0;\
+                }\
+                .alloy-selector-button:focus,\
+                .alloy-selector-button:hover {\
+                  border: 0;\
+                }\
+                .alloy-selector-body {\
+                  overflow: hidden;\
+                  position: relative;\
+                  padding: 0;\
+                  text-align: center;\
+                }\
+                .alloy-selector-line {\
+                  display: inline-block;\
+                  position: relative;\
+                  text-align: center;\
+                  vertical-align: top;\
+                  width: ' + w + '%;\
+                }\
+                .alloy-selector-line ul {\
+                  list-style: none;\
+                  margin: 91px 0 0 22%;\
+                  padding: 0;\
+                  width: 50%;\
+                  min-height: 256px;\
+                }\
+                .alloy-selector-line ul li {\
+                  height: 33px;\
+                  font-size: 23px;\
+                  white-space: nowrap;\
+                }\
+                .alloy-selector-mask {\
+                  background-color: rgba(0, 0, 0, 0.4);\
+                  position: absolute;\
+                  z-index: 999;\
+                  width: 100%;\
+                  left: 0;\
+                  top: 0;\
+                  bottom: 0;\
+                }\
+                .alloy-selector-submask1 {\
+                  background-color: rgba(255, 255, 255, 0.7);\
+                  position: absolute;\
+                  z-index: 1001;\
+                  width: 100%;\
+                  height: 89px;\
+                  top: 0px;\
+                  left: 0px;\
+                }\
+                .alloy-selector-submask2 {\
+                  background-color: rgba(255, 255, 255, 0.7);\
+                  position: absolute;\
+                  z-index: 1001;\
+                  width: 100%;\
+                  height: 90px;\
+                  top: 122px;\
+                  left: 0px;\
+                }' + t;
+    };
+
+    // for widget's stylesheet
+    function scoper(css) {
+        var id = generateID();
+        var prefix = "#" + id;
+        css = css.replace(/\/\*[\s\S]*?\*\//g, '');
+        var re = new RegExp("([^\r\n,{}]+)(,(?=[^}]*{)|\s*{)", "g");
+        css = css.replace(re, function(g0, g1, g2) {
+            if (g1.match(/^\s*(@media|@keyframes|to|from|@font-face)/)) {
+                return g1 + g2;
+            }
+            if (g1.match(/:scope/)) {
+                g1 = g1.replace(/([^\s]*):scope/, function(h0, h1) {
+                    if (h1 === "") {
+                        return "> *";
+                    } else {
+                        return "> " + h1;
+                    }
+                });
+            }
+            g1 = g1.replace(/^(\s*)/, "$1" + prefix + " ");
+            return g1 + g2;
+        });
+        addStyle(css,id+"-style");
+        return id;
+    }
+    function generateID() {
+        var id =  ("scoped"+ Math.random()).replace("0.","");
+        if(document.getElementById(id)){
+            return generateID();
+        }else {
+            return id;
+        }
+    }
+    var isIE = (function () {
+        var undef,
+            v = 3,
+            div = document.createElement('div'),
+            all = div.getElementsByTagName('i');
+        while (
+            div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->',
+                all[0]
+            );
+        return v > 4 ? v : undef;
+    }());
+    function addStyle(cssText, id) {
+        var d = document,
+            someThingStyles = d.createElement('style');
+        d.getElementsByTagName('head')[0].appendChild(someThingStyles);
+        someThingStyles.setAttribute('type', 'text/css');
+        someThingStyles.setAttribute('id', id);
+        if (isIE) {
+            someThingStyles.styleSheet.cssText = cssText;
+        } else {
+            someThingStyles.textContent = cssText;
+        }
+    }
+
     // create list
     function createList(parent, num) {
         var list = [];
@@ -41,7 +242,7 @@
     AlloyTouch.MultipleSelect = function (option) {
         this.renderTo = typeof option.renderTo === 'string' ? document.querySelector(option.renderTo) : option.renderTo;
         this.options = option.options;
-        this.level = option.level;
+        this.level = option.level || 1;
         this.complete = option.complete || _noop;
         this.change = option.change || _noop;
 
@@ -70,22 +271,22 @@
             if(!_nowSelected[i]) _nowSelected[i] = 0;
         }
 
-        var tpl = '<div class="alloy-selector">\
-                    <div class="alloy-selector-mask"></div>\
-                    <div class="alloy-selector-wrap">\
-                        <div class="alloy-selector-head jmu-border-1px border-bottom">\
-                            <div id="alloy-selector-complete-button-1" class="alloy-selector-button">完成</div>\
+        var id = scoper(getCss(this.level));
+        var tpl = '<div class="alloy-selector" id="' + id + '">\
+                        <div class="alloy-selector-mask"></div>\
+                        <div class="alloy-selector-wrap">\
+                            <div class="alloy-selector-head jmu-border-1px border-bottom">\
+                                <div id="alloy-selector-complete-button-1" class="alloy-selector-button">完成</div>\
+                            </div>\
+                            <div id="alloy-selector-body" class="alloy-selector-body">\
+                                <div class="alloy-selector-submask1 jmu-border-1px border-bottom"></div>\
+                                <div class="alloy-selector-submask2 jmu-border-1px border-top"></div>\
+                            </div>\
                         </div>\
-                        <div id="alloy-selector-body" class="alloy-selector-body">\
-                            <div class="alloy-selector-submask1 jmu-border-1px border-bottom"></div>\
-                            <div class="alloy-selector-submask2 jmu-border-1px border-top"></div>\
-                        </div>\
-                    </div>\
-                </div>';
+                    </div>';
 
         this.renderTo.insertAdjacentHTML('beforeEnd', tpl);
 
-        
         // mask.style.height = window.innerHeight + "px";
         
         // get selected arr
