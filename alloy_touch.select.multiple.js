7f172d1b3a8836bf1fa9e01e996ea0bdd8cb57a6
june_01 <juneandgreen@gmail.com>
2016-12-22 16:11:40 +0800

    fixed multiple select

--- alloy_touch.select.multiple.js
+++ alloy_touch.select.multiple.js
@@ -1,21 +1,73 @@
 (function () {
     if(!AlloyTouch) throw new Error('you need to include AlloyTouch!');
 
+    if(!Transform) throw new Error('you need to include Transform!');
+
+    // create list
+    function createList(parent, num) {
+        var list = [];
+        for(var i=0; i<num; i++) {
+            var div = document.createElement('div');
+            div.id = 'alloy-selector-line-1-' + i;
+            div.className = 'alloy-selector-line';
+
+            var ul = document.createElement('ul');
+            div.appendChild(ul);
+
+            Transform(div);
+            list.push(div);
+            parent.appendChild(div);
+        }
+
+        return list;
+    }
+
+    // create touch area
+    function createTouch(parent, num) {
+        var touches = [];
+        for(var i=0; i<num; i++) {
+            var div = document.createElement('div');
+            div.className = 'alloy-selector-touch' + i;
+
+            touches.push(div);
+            parent.appendChild(div);
+        }
+
+        return touches;
+    }
+
+    function _noop() {}
+
     AlloyTouch.MultipleSelect = function (option) {
         this.renderTo = typeof option.renderTo === 'string' ? document.querySelector(option.renderTo) : option.renderTo;
-        this.length = option.options.length;
         this.options = option.options;
+        this.level = option.level;
+        this.complete = option.complete || _noop;
+        this.change = option.change || _noop;
 
         var that = this;
-        var initSel = option.selectedIndex;
-        
-        var i = 0;
-        var arrProv = [],arrCity=[];
-        for (; i < this.length; i++) {
-            var item =this.options[i];
-            if (initSel[0] === i) {
-                arrCity = item.list;
+        var _nowSelected = option.selectedIndex || [];
+
+        // merge options list
+        function _mergeListInfo(list) {
+            var arr = [];
+            var info = {};
+            for(var i=0,len=list.length; i<len; i++) {
+                arr.push('<li>' + list[i].name + '</li>');
+
+                info[i] = list[i].list ? _mergeListInfo(list[i].list) : {length: 0};
             }
+            info.length = i;
+            info.content = arr.join('');
+
+            return info;
+        }
+
+        // init options
+        var _cache = _mergeListInfo(this.options);
+        // init selected
+        for(var i=0; i<this.level; i++) {
+            if(!_nowSelected[i]) _nowSelected[i] = 0;
         }
 
         var tpl = '<div class="alloy-selector">\
@@ -24,118 +76,95 @@
                         <div class="alloy-selector-head jmu-border-1px border-bottom">\
                             <div id="alloy-selector-complete-button-1" class="alloy-selector-button">完成</div>\
                         </div>\
-                        <div class="alloy-selector-body">\
+                        <div id="alloy-selector-body" class="alloy-selector-body">\
                             <div class="alloy-selector-submask1 jmu-border-1px border-bottom"></div>\
                             <div class="alloy-selector-submask2 jmu-border-1px border-top"></div>\
-                            <div id="alloy-selector-line-1-0" class="alloy-selector-line">\
-                                <ul></ul>\
-                            </div>\
-                            <div id="alloy-selector-line-1-1" class="alloy-selector-line">\
-                                <ul></ul>\
-                            </div>\
-                            <div class="alloy-selector-left"></div>\
-                            <div class="alloy-selector-right"></div>\
                         </div>\
                     </div>\
                 </div>';
 
         this.renderTo.insertAdjacentHTML('beforeEnd', tpl);
 
-       
-        for (var p in this.options) {
-            arrProv.push(p);
-         
-        }
-
-        var _selectedP = '';
-        var _selectedC = '';
-        var _p2cCache = {};
+        
         // mask.style.height = window.innerHeight + "px";
+        
+        // get selected arr
+        function getSelectedArr() {
+            var arr = [_nowSelected];
+            var options = that.options || [];
+            _nowSelected.forEach(function(sel) {
+                options = options[sel] || {};
+                arr.push(options && options.name);
+                options = options.list || [];
+            });
+
+            return arr;
+        }
         
         // enter btn
         var completeBtn = document.getElementById('alloy-selector-complete-button-1');
         completeBtn.addEventListener('touchend', function (e) {
-            alert(_selectedP + '-' + _selectedC);
+            that.complete.apply(that, getSelectedArr());
             e.stopPropagation();
             e.preventDefault();
         }, false);
 
-        var list = document.querySelectorAll('.alloy-selector-line');
+        var parent = document.getElementById('alloy-selector-body');
+        var list = createList(parent, this.level);
+        var touches = createTouch(parent, this.level);
+        var atList = {};
 
-        // init list1
-        var list1 = list[0];
-        var arr1 = [];
-        var arrProv = [];
-        for (var p in this.options) {
-            if (!_selectedP) {
-                _selectedP = p;
-            }
-            arrProv.push(p);
-            arr1.push('<li>' + p + '</li>');
+        // init list
+        for(var l=0; l<this.level; l++) {
+            var info = _getList.call(this, l);
+            list[l].querySelector('ul').innerHTML = info.content;
+
+            var listInit = info[_nowSelected[l]] ? _nowSelected[l] : 0;
+            _nowSelected[l] = listInit;
+            var at = new AlloyTouch({
+                touch: touches[l], // 反馈触摸的dom
+                target: list[l], // 运动的对象
+                initialVaule: listInit * -33,
+                property: 'translateY',  // 被滚动的属性
+                min: -33 * (info.length - 1), // 不必需,滚动属性的最小值
+                max: 0,
+                animationEnd: (function(l) {
+                    return function(value) {
+                        _nowSelected[l] = Math.round(Math.abs(value / 33));
+
+                        if(l === that.level-1) return; // the last list has not children
+
+                        for(var j=l+1; j < that.level; j++) {
+                            var childrenInfo = _getList.call(that, j);
+                            _nowSelected[j] = 0;
+                            list[j].querySelector('ul').innerHTML = childrenInfo.content || '';
+                            atList[j].min = -33 * (childrenInfo.length - 1);
+                            list[j].translateY = 0;
+                        }
+
+                        that.change.apply(that, getSelectedArr());
+                    }
+                })(l),
+                step: 33
+            });
+
+            atList[l] = at;
         }
-        list1.querySelector('ul').innerHTML = arr1.join('');
 
-        // init list2
-        var list2 = list[1];
-        var cityInfo = _getCityList.call(that);
-        _selectedC = cityInfo[0];
-        list2.querySelector('ul').innerHTML = cityInfo.content;
+        // get options list
+        function _getList(levelIndex) {
+            levelIndex = levelIndex || 0;
+            var parentSelected = _nowSelected[0] || 0;
+            var cache = _cache;
 
-        var leftDiv = document.querySelector('.alloy-selector-left');
-        var rightDiv = document.querySelector('.alloy-selector-right');
-        Transform(list1);
-        Transform(list2);
+            for(var i=0; i<levelIndex; i++) {
+                parentSelected = _nowSelected[i];
+                cache = cache[parentSelected];
 
-        var list1Init = arrProv[initSel[0]] ? initSel[0] : 0;
-        var at1 = new AlloyTouch({
-            touch: leftDiv, // 反馈触摸的dom
-            target: list1, // 运动的对象
-            initialVaule: list1Init * -33,
-            property: "translateY",  // 被滚动的属性
-            min: -33 * (arrProv.length - 1), // 不必需,滚动属性的最小值
-            max: 0,
-            animationEnd: function (value) {
-                // console.log(value)
-                // console.log(Math.abs(value / 33))
-                _selectedP = arrProv[Math.round(Math.abs(value / 33))];
-                var cityInfo = _getCityList.call(that);
-
-                _selectedC = cityInfo[0];
-                list2.querySelector("ul").innerHTML = cityInfo.content;
-                at2.min = -33 * (cityInfo.length - 1);
-                list2.translateY = 0;
-            },
-            step: 33
-        });
-        var list2Init = arrProv[initSel[0]] && this.options[arrProv[initSel[0]]][initSel[1]] ? initSel[1] : 0;
-        var at2 = new AlloyTouch({
-            touch: rightDiv, // 反馈触摸的dom
-            target: list2, // 运动的对象
-            initialVaule: list2Init * -33,
-            property: "translateY",  // 被滚动的属性
-            min: -33 * (cityInfo.length - 1), // 不必需,滚动属性的最小值
-            max: 0,
-            animationEnd: function (value) {
-                _selectedC = _getCityList.call(that)[Math.round(Math.abs(value / 33))];
-                //console.log(_selectedC);
-            },
-            step: 33
-        });
-
-
-        function _getCityList() {
-            if (!_p2cCache[_selectedP]) {
-                var arr = [];
-                var info = {};
-                for (var i = 0; i < this.options[_selectedP].length; ++i) {
-                    info[i] = this.options[_selectedP][i];
-                    arr.push('<li>' + info[i] + '</li>');
-                }
-                info.length = i;
-                info.content = arr.join('')
-                _p2cCache[_selectedP] = info;
+                if(!cache) return {length: 0};
             }
-            return _p2cCache[_selectedP];
+
+            return cache;
         }
     }
     
