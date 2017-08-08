3c22a4f5b1cbb39588777429185e0142d5879524
dntzhang <kmdjs@qq.com>
2017-08-08 10:27:07 +0800

    db check

--- transformjs/transform.js
+++ transformjs/transform.js
@@ -234,13 +234,13 @@
     }
 
     function Transform(obj, notPerspective) {
-        if(obj.hasOwnProperty("translateX")) return;
+        if(obj.___mixCSS3Transform) return;
         var observeProps = ["translateX", "translateY", "translateZ", "scaleX", "scaleY", "scaleZ", "rotateX", "rotateY", "rotateZ", "skewX", "skewY", "originX", "originY", "originZ"],
             objIsElement = isElement(obj);
         if (!notPerspective) {
             observeProps.push("perspective");
         }
-
+        obj.___mixCSS3Transform = true
         observe(
             obj,
             observeProps,
